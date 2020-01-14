624512282c42cb4f01b22c9769c5c899a3b23294
lbmmrt <karinawowo@mail.ru>
2020-01-14 19:02:22 +0300

    Initial Commit

---
+++ Cafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cafe
+//
+//  Created by Карина Паланчук on 14/01/2020.
+//  Copyright © 2020 Karina Palanchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
