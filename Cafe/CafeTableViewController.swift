58286ffc10c8a298d9df6bf463622685c52c66fd
lbmmrt <lbmmrt@gmail.com>
2020-01-18 20:19:12 +0300

    added alertController for phone call

--- Cafe/CafeTableViewController.swift
+++ Cafe/CafeTableViewController.swift
@@ -41,57 +41,37 @@ class CafeTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CafeTableViewCell
         
         cell.imageCafe.image = UIImage(named: restaurantImages[indexPath.row])
-        cell.imageCafe.layer.cornerRadius = 37.5
+        cell.imageCafe.layer.cornerRadius = 40
         cell.imageCafe.clipsToBounds = true
         cell.nameLebel.text = restaurantNames[indexPath.row]
         
         return cell
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    func showAlert(index: Int) {
+        
+        let ac = UIAlertController(title: nil, message: "Выберите дейсвие", preferredStyle: .actionSheet)
+        let call = UIAlertAction(title: "Позвонить: 8 (999) 666-11\(index)", style: .default) {
+            UIAlertAction in
+            showAlert2()
+        }
+        let cancel = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        ac.addAction(cancel)
+        ac.addAction(call)
+        present(ac, animated: true, completion: nil)
      
-     }
-     */
     
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
+    func showAlert2() {
+        let cancelCall = UIAlertController(title: nil, message: "Вызов невозможен", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        cancelCall.addAction(ok)
+        present(cancelCall, animated: true, completion: nil)
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showAlert(index: indexPath.row)
+    }
+ 
 }
