4e64a329df8cf5f7535af5238c36fce904172874
lbmmrt <lbmmrt@gmail.com>
2020-01-27 20:15:42 +0300

    animation for rating buttons

---
+++ Cafe/RateViewController.swift
@@ -0,0 +1,61 @@
+//
+//  RateViewController.swift
+//  Cafe
+//
+//  Created by Карина Паланчук on 27/01/2020.
+//  Copyright © 2020 Karina Palanchuk. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ratingStackView: UIStackView!
+    @IBOutlet weak var badButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var briliantBotton: UIButton!
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        UIView.animate(withDuration: 0.4) {
+//        self.ratingStackView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+//        }
+        
+        let buttonArray = [badButton, goodButton, briliantBotton]
+        for (index, button) in buttonArray.enumerated() {
+            let delay = Double(index) * 0.2
+            UIView.animate(withDuration: 0.6, delay: delay, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                button?.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        badButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        goodButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        briliantBotton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.insertSubview(blurEffectView, at: 1)
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
